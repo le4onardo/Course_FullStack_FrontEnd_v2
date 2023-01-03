3d8e7f603c5a0479ddfa5bddeae248448ea24834
Leonardo Julio Rios Aliaga <44624540+le4onardo@users.noreply.github.com>
2023-01-03 01:05:23 -0400

    add web socket on same port

--- app/app.js
+++ app/app.js
@@ -1,15 +1,35 @@
-const express = require('express')
+const { WebSocketServer } = require('ws');
+const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
+const server  = require('http').createServer(app);
+
+
+app.use('/js', express.static(path.join(__dirname, 'ui/js/')));
+
 app.get('/', (req, res)=> {
-
-res.send('Hello World!');
-
+   res.sendFile(path.join(__dirname + '/ui/html/index.html'));
 });
 
 app.get('/teapot', (req, res)=>{
-res.set('x-custom-header', 'I am teapot');
-res.status(418);
-res.send('I prefer coffe');
+	res.set('x-custom-header', 'I am teapot');
+	res.status(418);
+	res.send('I prefer coffe');
 })
-app.listen(port, ()=> console.log(`Example app listening on port ${port}!`));
+
+
+
+const wss = new WebSocketServer({
+ server: server,
+ path: '/web_socket',
+});
+
+wss.on('connection', function connection(ws) {
+ ws.on('message', function message(data) {
+   console.log('message received', data); 
+   ws.send('awesome');
+ });
+});
+
+server.listen(port, ()=> console.log(`Example app listening on port ${port}!`));
